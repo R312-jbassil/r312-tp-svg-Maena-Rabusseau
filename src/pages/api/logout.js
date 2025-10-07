f6f2157cb7574db526452e5d6eec45202ef4efc7
Maena-Rabusseau <maena.rabusseau@edu.univ-fcomte.fr>
2025-10-07 11:41:19 +0200

    Ajout utilisateurs et connexion réussi

---
+++ src/pages/api/logout.js
@@ -0,0 +1,7 @@
+import  pb  from "../../utils/pb";
+import { Collections } from "../../utils/pocketbase-types";
+
+export const POST = async ({ cookies }) => {
+  cookies.delete("pb_auth", { path: "/" });
+  return new Response(null,{ status: 303, headers: {Location: '/'} });
+}; 
